57a2e886525fd189606835ce030dc51baa284b43
sanchezzzhak <sanchezzzhak@ya.ru>
2022-12-01 15:52:53 +0300

    feat(misc) added update readme tool

--- misc/update-readme.php
+++ misc/update-readme.php
@@ -13,8 +13,22 @@ $content = file_get_contents($file);
 
 $cardTypes = BankInfo::getCardTypes();
 $cardTypeCount = count($cardTypes);
+natcasesort($cardTypes);
 $cardTypeList = implode(', ', $cardTypes);
-$list1 = "##### Support detect browsers list ($cardTypeCount):\n\n<details>\n<summary>Show details</summary>\n\n $cardTypeList\n</details>";
-$content = preg_replace("/(^#{5} List of card types(?:.*?)<\/details>)/ims", $list1, $content);
 
-file_get_contents($file, $content);
+$contentCardType = "##### List of card types($cardTypeCount):\n\n<details>\n<summary>Show details</summary>\n\n $cardTypeList\n</details>";
+$content = preg_replace("~(^#{5} List of card types ?(?:.*?)</details>)~ims", $contentCardType, $content, 1);
+
+$banks = BankInfo::getBanks();
+$bankCount = count($banks);
+$banksUniques = [];
+foreach ($banks as $bank) {
+    $banksUniques[] = sprintf('%s (%s)', $bank['enTitle'], $bank['country']);
+}
+$banksUniques = array_unique($banksUniques);
+natcasesort($banksUniques);
+$bankList = implode(', ', $banksUniques);
+$contentBanks = "##### List of banks($bankCount):\n\n<details>\n<summary>Show details</summary>\n\n $bankList\n</details>";
+$content = preg_replace("~(^#{5} List of banks ?(?:.*?)</details>)~ims", $contentBanks, $content, 1);
+
+file_put_contents($file, $content);
